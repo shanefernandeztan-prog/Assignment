1b349ded0532cfc18162c7a11f96128bea3192d4
shanefernandeztan-prog <shanefernandeztan@gmail.com>
2026-02-28 01:24:08 +0800

    Add files via upload

---
+++ Tan2.py
@@ -0,0 +1,15 @@
+import scipy.stats as sts
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu = np.linspace(1.65, 1.8, num =50)
+def likelihood_func(datum, mu):
+    likelihood_out = sts.norm.pdf(datum, mu, scale = 0.1) 
+    return likelihood_out / likelihood_out.sum()
+
+likelihood_out = likelihood_func(1.7, mu)
+plt.plot(mu, likelihood_out)
+plt.title(r"Likelihood of $\mu$ given observation 1.7m")
+plt.ylabel("Probability Density/Likelihood")
+plt.xlabel(r"Value of $\mu$")
+plt.show()
